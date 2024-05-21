ccc251ea1987b3dd8b850b36ee5b72fb47638e85
tracer-ics2023 <tracer@njuics.org>
2024-05-21 16:11:55 +0800

    >  compile NEMU 2024 yjc Linux ubuntux86 6.7.11-orbstack-00143-ge6b82e26cd22 #1 SMP Sat Mar 30 12:20:36 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:11:55 up  4:48,  2 users,  load average: 0.00, 0.02, 0.00

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -56,6 +56,12 @@ static int cmd_q(char *args) {
   /*return 0;*/
 }
 
+static int cmd_si(char *args) {
+  // extract the first argument
+  cpu_exec(1);
+  return 0;
+}
+
 static int cmd_help(char *args);
 
 static struct {
@@ -66,6 +72,7 @@ static struct {
     {"help", "Display information about all supported commands", cmd_help},
     {"c", "Continue the execution of the program", cmd_c},
     {"q", "Exit NEMU", cmd_q},
+    {"si", "step", cmd_si},
 
     /* TODO: Add more commands */
 
