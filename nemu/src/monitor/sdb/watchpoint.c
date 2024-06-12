92f9ff20f1f082634a3f535e7d3543120437cbc9
tracer-ics2023 <tracer@njuics.org>
2024-06-12 23:05:08 +0800

    >  compile NEMU 2024 yjc Linux ubuntux86 6.7.11-orbstack-00143-ge6b82e26cd22 #1 SMP Sat Mar 30 12:20:36 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:05:08 up 20:03,  3 users,  load average: 1.16, 1.09, 1.02

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -82,6 +82,7 @@ void deleteWP(uint32_t N, bool *success) {
   WP *n_head = head;
   uint32_t i = 0;
   while (i < N && n_head != free_) {
+    printf("into loop!!\n");
     if (N == i + 1) {
       WP *temp = n_head->next;
       // nullpointer exception
