f136df1cb84e65295130dccb0af45ecf53cb82cd
tracer-ics2023 <tracer@njuics.org>
2024-06-13 12:42:32 +0800

    >  compile NEMU 2024 yjc Linux ubuntux86 6.7.11-orbstack-00143-ge6b82e26cd22 #1 SMP Sat Mar 30 12:20:36 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:42:32 up  1:54,  3 users,  load average: 0.26, 0.13, 0.04

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -43,6 +43,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
   // watch point
+  //
 }
 
 static void exec_once(Decode *s, vaddr_t pc) {
